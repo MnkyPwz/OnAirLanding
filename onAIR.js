8d79f8d3f96bec45cdb6af9acbbeb847b1f999ae
CallAndGus <preston@montanamountainoutfitters.com>
2020-03-31 23:07:20 -0600

    darker computer animation

--- onAIR.js
+++ onAIR.js
@@ -1,3 +1,36 @@
-$("#button").click(function() {
-    $('.transform').toggleClass('transform-active');
-  });
+var play = document.querySelector("#play");
+var reverse = document.querySelector("#reverse");
+var restart = document.querySelector("#restart");
+
+var tl = gsap.timeline({paused: true});
+
+
+tl.to(".green", {duration: .5, x: 12})
+tl.to(".computerFrame", {opacity:1})
+tl.to(".screw3", {duration: 1, opacity:1})
+tl.to(".computer", {opacity:1})
+tl.to(".screw4", {duration: 1, opacity:1})
+tl.to(".screw5", {duration: 1, opacity:1})
+tl.to(".powerIcon", {opacity:1})
+
+
+tl.to(".screenBox", {opacity:1})
+
+tl.to(".screen", {duration: 1.5, opacity:1})
+tl.to(".behind", {opacity:1})
+tl.to(".frame", {opacity:1})
+
+
+
+var tl2 =gsap.timeline({paused:true});
+
+tl2.to(".screen2", {boxShadow:" 0 0 25px 15px #a01ea0"})
+
+
+play.onclick = function() {
+  tl.play();
+}
+
+reverse.onclick = function() {
+  tl.reverse();
+}
